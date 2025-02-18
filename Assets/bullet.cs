43bc0dac65c4b71f9dd0dc029532b65a8bd418b9
James Edwards <jamesedwards04@gmail.com>
2025-02-18 13:50:54 +0000

    Bullet Despawning

--- Assets/bullet.cs
+++ Assets/bullet.cs
@@ -1,10 +1,12 @@
+using System.Collections;
 using UnityEngine;
 
 public class bullet : MonoBehaviour
 {
     public float speed = 10f;
     public int damage = 10;
-
+    public float despawnSeconds = 1;
+    
     private Rigidbody2D rb;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -12,11 +14,20 @@ public class bullet : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         rb.AddForce(transform.right * speed * 100);
+
+        StartCoroutine(DespawnTimer(despawnSeconds));
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    IEnumerator DespawnTimer(float despawnSeconds)
+    {
+        yield return new WaitForSecondsRealtime(despawnSeconds);
+
+        Destroy(gameObject);
     }
 }
